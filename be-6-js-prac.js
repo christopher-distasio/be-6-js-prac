975e2b608bbacea281ee13c0b0116dfb715cc793
Chris DiStasio <94086919+christopher-distasio@users.noreply.github.com>
2023-11-17 22:23:42 -0600

    Solves last three

--- be-6-js-prac.js
+++ be-6-js-prac.js
@@ -33,8 +33,23 @@ let contacts = [
 
 // 1. Loop through the contacts array and console.log the name of each person
 
+for (let i = 0; i < contacts.length; i++) {
+    console.log(contacts[i].name);
+}
+
+// contacts.forEach(contact => {
+//     console.log(contact.name);
+// });
+
+
 // 2. Loop through the contacts array and filter out everyone who has an instagram account
 
+let noInstagram = contacts.filter(function(contact) {
+  return contact.social_media.instagram === undefined;
+});
 
 
+// 3. Using the given contacts array, use 'Math.random' to randomly select a contact from the array and save it to a variable called 'randomContact.'
 
+let randomIndex = Math.floor(Math.random() * contacts.length);
+let randomContact = contacts[randomIndex];
