ee59db0bf26d2e1c852b9389d917146100a00940
Chris DiStasio <94086919+christopher-distasio@users.noreply.github.com>
2023-11-17 21:40:50 -0600

    Adds file

---
+++ be-6-js-prac.js
@@ -0,0 +1,40 @@
+let contacts = [
+  {
+      name: "Jane Doe",
+      age: 21,
+      social_media: {
+          instagram: "jane.doe",
+          twitter: "jane_doe"
+      }
+  },
+  {
+      name: "John Doe",
+      age: 21,
+      social_media: {
+          instagram: "john.doe",
+          twitter: "john_doe"
+      }
+  },
+  {
+      name: "Mary Deer",
+      age: 21,
+      social_media: {
+          twitter: "mary_deer"
+      }
+  },
+  {
+      name: "Gary Deer",
+      age: 21,
+      social_media: {
+          twitter: "gary_deer"
+      }
+  }
+]
+
+// 1. Loop through the contacts array and console.log the name of each person
+
+// 2. Loop through the contacts array and filter out everyone who has an instagram account
+
+
+
+
